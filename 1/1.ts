41fbbd02d0988c2a9bf81b58cad5d7325490d59c
Pascal Haupt <75530910+haupt-pascal@users.noreply.github.com>
2024-12-01 10:59:18 +0100

    finished day one aoc

---
+++ 1/1.ts
@@ -0,0 +1,27 @@
+const file = Bun.file("input.txt")
+const fileContent = await file.text()
+const lines = fileContent.split("\n")
+const leftNumber = lines.map((l: string) => Number.parseInt(l.split(" ")[0]))
+const rightNumber = lines.map((r: string) => Number.parseInt(r.split("  ")[1]))
+const leftNumbersSorted = leftNumber.sort((n1, n2) => n1 - n2);
+const rightNumbersSorted = rightNumber.sort((n1,n2) => n1 - n2);
+//const pairs = leftNumber.map((l: number, i: number) => [l, rightNumber[i]])
+const sortedPairs = leftNumbersSorted.map((l:number, i:number) => [l, rightNumbersSorted[i]])
+//console.log(pairs)
+console.log(sortedPairs)
+const sumArray = []
+for (let i=0; i<sortedPairs.length; i++) {
+    const leftNumber = sortedPairs[i][0]
+    const rightNumber = sortedPairs[i][1]
+    if (leftNumber > rightNumber) {
+        const sum = leftNumber - rightNumber
+        sumArray.push(sum)
+    } else {
+        const sum = rightNumber - leftNumber
+        sumArray.push(sum)
+    }
+}
+
+//console.log(sumArray)
+const sum = sumArray.reduce((partialSum, a) => partialSum + a, 0)
+console.log(sum)
---
+++ 1/2.ts
@@ -0,0 +1,32 @@
+/**
+* Wie oft kommt die Nummer der linken Liste in der rechten Liste vor
+*   Mulitpliziere jede Nummer der linken Liste x wie oft sie in der rechten Liste vorkommt
+*   Diese Zahl wird auf den similarityScore addiert  
+*/
+
+const file = Bun.file("input.txt")
+const fileContent = await file.text()
+const lines = fileContent.split("\n")
+const leftNumber = lines.map((l: string) => Number.parseInt(l.split(" ")[0]))
+const rightNumber = lines.map((r: string) => Number.parseInt(r.split("  ")[1]))
+const leftNumbersSorted = leftNumber.sort((n1, n2) => n1 - n2);
+const rightNumbersSorted = rightNumber.sort((n1,n2) => n1 - n2);
+
+const sumArray = []
+
+function getOccurences(array: number[], value: number) {
+    let count = 0;
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    array.forEach((v) => (v === value && count++));
+    return count;
+}
+
+for (let i=0; i<leftNumbersSorted.length; i++) {
+    const leftNumber = leftNumbersSorted[i]
+    const occurences = getOccurences(rightNumbersSorted, leftNumber)
+    const similarityScore = leftNumber * getOccurences(rightNumbersSorted, leftNumber)
+    sumArray.push(similarityScore)
+}
+
+const sum = sumArray.reduce((partialSum, a) => partialSum + a, 0)
+console.log(sum)
